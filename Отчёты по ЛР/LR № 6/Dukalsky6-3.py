12f479ffdbb8ace98289674a65d485a361924a0f
KOMERATTEN <vladimirdukalsky17@mail.ru>
2025-10-09 01:36:25 +0500

    Delete Отчёты по ЛР directory

--- Отчёты по ЛР/LR № 6/Dukalsky6-3.py
+++
@@ -1,3 +0,0 @@
-text = input("\nВведите текст: ")
-step = int(input("Введите шаг: "))
-print(f"Текст с шагом: {text[::step]}")
--- Отчёты по ЛР/LR № 6/Dukalsky6-4.py
+++
@@ -1,3 +0,0 @@
-text = input("Введите текст: ")
-start, end = map(int, input("Введите диапазон (например: 2 5): ").split())
-print(f"Подстрока: {text[start-1:end]}")
