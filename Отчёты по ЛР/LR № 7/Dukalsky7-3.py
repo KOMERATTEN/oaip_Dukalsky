735abe630030858186b76b636872eea3d39502e9
KOMERATTEN <vladimirkrono85@gmail.com>
2025-10-14 00:18:35 +0500

    Delete Отчёты по ЛР directory

--- Отчёты по ЛР/LR № 7/Dukalsky7-3.py
+++
@@ -1,4 +0,0 @@
-text = input("Введите текст: ")
-word = input("Введите слово: ")
-count = text.count(word)
-print(f"Слово '{word}' {'найдено' if word in text else 'не найдено'}, встречается {count} раз")
