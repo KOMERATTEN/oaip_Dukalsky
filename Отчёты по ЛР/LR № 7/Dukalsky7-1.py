5a422c5f8d2b6316fd2678e1936b74e4803c92f8
KOMERATTEN <vladimirkrono85@gmail.com>
2025-10-14 00:15:21 +0500

    Delete Отчёты по ЛР directory

--- Отчёты по ЛР/LR № 7/Dukalsky7-1.py
+++
@@ -1,3 +0,0 @@
-direction = input("Введите направление (left/right/straight/back): ")
-actions = {"left": "влево", "right": "вправо", "straight": "прямо", "back": "назад"}
-print(f"Иду {actions[direction]}" if direction in actions else "Неправильное направление")
